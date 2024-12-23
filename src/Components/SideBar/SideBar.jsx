c32bfc75e5a82cf15f58a502c0397496a345988a
vinhraudaklak <vinhpro0972@gmail.com>
2024-12-23 23:27:50 +0700

    Xây dựng UI phần Header #6

--- src/Components/SideBar/SideBar.jsx
+++ src/Components/SideBar/SideBar.jsx
@@ -1,14 +1,14 @@
 import styles from './Sidebar.module.scss';
 import classNames from 'classnames/bind';
 
-const cx = classNames.bind(styles)
+const cx = classNames.bind(styles);
 
-const SideBar = () => {
-  return (
-    <aside className={cx('wrapper')}>
-      <h2>Side bar</h2>
-    </aside>
-  )
-}
+const Sidebar = () => {
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Side bar</h2>
+        </aside>
+    );
+};
 
-export default SideBar
+export default Sidebar;
